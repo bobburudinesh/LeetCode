53b481cb84186675ce3b4cbb3bf96479415c4237
Dinesh Bobburu <54496857+bobburudinesh@users.noreply.github.com>
2025-03-07 11:12:29 -0600

    Time: 0 ms (100.00%) | Memory: 12.1 MB (94.83%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.c
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.c
@@ -1,13 +1,35 @@
+void swap(int* a, int* b) {
+    int temp = *b;
+    *b = *a;
+    *a = temp;
+}
 int removeDuplicates(int* nums, int numsSize) {
     if(numsSize <1) {
         return 0;
     }
-    int uniqueNums = 1;
+    // int uniqueNums = 1;
+    // for(int i = 1; i<numsSize; i++) {
+    //     if(nums[i] > nums[i-1]) {
+    //         nums[uniqueNums] = nums[i];
+    //         uniqueNums++;
+    //     } 
+    // }
+    // return uniqueNums;
+
+    // Above is solution 1
+
+    //Below is solution 2
+    int *p; // iterating pointer
+    int *q;
+    p = nums;
+    q = nums;
+    p++;
     for(int i = 1; i<numsSize; i++) {
-        if(nums[i] > nums[i-1]) {
-            nums[uniqueNums] = nums[i];
-            uniqueNums++;
+        if(*p != *q) {
+            q++;
+            *q = *p;
         } 
+        p++;
     }
-    return uniqueNums;
+    return ((int)(q-nums) + 1);
 }
