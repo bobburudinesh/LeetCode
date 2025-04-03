15e4febf6eac069fa8b8d565e479c10d28aa77bc
Dinesh Bobburu <54496857+bobburudinesh@users.noreply.github.com>
2025-04-03 15:58:43 -0500

    Time: 0 ms (100.00%) | Memory: 15.4 MB (42.60%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.c
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.c
@@ -1,17 +1,15 @@
 int maxProfit(int* prices, int pricesSize) {
-    int sell = 0, buy = INT_MAX;
-    int profit = 0, max = INT_MIN;
+    int buy = INT_MAX, profit = 0, max = 0;
     for(int i = 0; i< pricesSize; i++) {
         if(prices[i] <= buy) {
             buy = prices[i];
         } else {
-            sell = prices[i];
-            profit = sell - buy;
+            profit = prices[i] - buy;
             if(profit > max) {
                 max = profit;
             }
         }
         
     }
-    return sell ? max : 0;
+    return max;
 }
