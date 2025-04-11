1c9c5d06d61bd924546f970f96af7f649bf35a96
Dinesh Bobburu <54496857+bobburudinesh@users.noreply.github.com>
2025-04-10 23:15:02 -0500

    Time: 135 ms (3.53%) | Memory: 24.1 MB (38.24%) - LeetSync

--- 89-gray-code/gray-code.c
+++ 89-gray-code/gray-code.c
@@ -5,8 +5,15 @@ int bin_grey(int n) {
     return n ^ (n >> 1);
 } 
 int* grayCode(int n, int* returnSize) {
+
     *returnSize = (1 << (n));
     int* result = (int*)malloc(sizeof(int) * (*returnSize));
+    // base case
+    if (n == 1) {
+        result[0] = 0;
+        result[1] = 1;
+        return result;
+    }
     int count = 0;
     while(count < (*returnSize)) {
          result[count] = bin_grey(count);
