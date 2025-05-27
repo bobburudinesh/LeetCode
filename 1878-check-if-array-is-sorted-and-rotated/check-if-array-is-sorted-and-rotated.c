bb9ca8857fa7603fb8d5d820f979bbe5d4b3448a
Dinesh Bobburu <54496857+bobburudinesh@users.noreply.github.com>
2025-05-27 11:33:11 -0500

    Time: 0 ms (100.00%) | Memory: 8.3 MB (7.47%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.c
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.c
@@ -4,19 +4,6 @@ bool check(int* nums, int numsSize) {
         if(nums[i-1] > nums[i]) {
             shift++;
         }
-        // if((nums[i] >= nums[i-1]) && (!shift) ) {
-        //     temp = nums[i];
-        // } else if (shift && (nums[0] >= nums[i]) && (nums[i] >= nums[i-1])) {
-        //     temp = nums[i];
-        // } else if((!shift) && (nums[i] < nums[i-1]))  {
-        //     if((nums[0] < nums[i])) {
-        //         return false;
-        //     }
-        //     shift = i;
-        //     continue;
-        // } else {
-        //     return false;
-        // }
     }
     if(nums[numsSize-1] > nums[0]) {
             shift++;
