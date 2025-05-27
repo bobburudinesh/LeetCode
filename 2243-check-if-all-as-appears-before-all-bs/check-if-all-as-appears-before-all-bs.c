d531ce203a318240b657f649781036d4d3e1021f
Dinesh Bobburu <54496857+bobburudinesh@users.noreply.github.com>
2025-05-27 11:47:02 -0500

    Time: 0 ms (100.00%) | Memory: 7.8 MB (34.55%) - LeetSync

--- 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.c
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.c
@@ -1,13 +1,9 @@
 bool checkString(char* s) {
     int length = strlen(s);
     for(int i = 1; i<length; i++) {
-        printf("i: %d, %c < %c\n", i, s[i], s[i-1]);
         if(s[i] < s[i-1]) {
             return false;
         }
     }
-    // if(s[length-1] < s[length-2]) {
-    //         return false;
-    // }
     return true;
 }
