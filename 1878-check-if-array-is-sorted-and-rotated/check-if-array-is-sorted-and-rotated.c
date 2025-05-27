a2faa95a2bfcf521a34e9569360f23a947ef0c62
Dinesh Bobburu <54496857+bobburudinesh@users.noreply.github.com>
2025-05-27 11:32:58 -0500

    Time: 0 ms (100.00%) | Memory: 8 MB (74.68%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.c
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.c
@@ -1,28 +1,25 @@
 bool check(int* nums, int numsSize) {
-    int temp = INT_MAX;
     int shift = 0;
     for(int i = 1; i<numsSize; i++) {
-        if((nums[i] >= nums[i-1]) && (!shift) ) {
-            temp = nums[i];
-        } else if (shift && (nums[0] >= nums[i]) && (nums[i] >= nums[i-1])) {
-            temp = nums[i];
-        } else if((!shift) && (nums[i] < nums[i-1]))  {
-            if((nums[0] < nums[i])) {
-                return false;
-            }
-            shift = i;
-            continue;
-        } else {
-            return false;
+        if(nums[i-1] > nums[i]) {
+            shift++;
         }
+        // if((nums[i] >= nums[i-1]) && (!shift) ) {
+        //     temp = nums[i];
+        // } else if (shift && (nums[0] >= nums[i]) && (nums[i] >= nums[i-1])) {
+        //     temp = nums[i];
+        // } else if((!shift) && (nums[i] < nums[i-1]))  {
+        //     if((nums[0] < nums[i])) {
+        //         return false;
+        //     }
+        //     shift = i;
+        //     continue;
+        // } else {
+        //     return false;
+        // }
     }
-    // for(i = shift; i< numsSize; i++) {
-    //     if(nums[i] >= nums[i-1]) {
-    //         temp = nums[i];
-    //     } else {
-    //         break;
-    //     }
-    // }
-
-    return true;
+    if(nums[numsSize-1] > nums[0]) {
+            shift++;
+    }
+    return shift <= 1;
 }
