8def62b72d11fa7f7843d0b8da98e5336f657289
Dinesh Bobburu <54496857+bobburudinesh@users.noreply.github.com>
2025-05-29 21:06:24 -0500

    Time: 0 ms (100.00%) | Memory: 8.8 MB (83.57%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.c
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.c
@@ -9,20 +9,37 @@ struct ListNode* swapPairs(struct ListNode* head) {
     if((!head) || (!head->next)) {
         return head;
     }
-    struct ListNode *p, *q, *dummy;
-    int temp;
-    p = head;
-    q = head->next;
-    while(q){
-        temp = p->val;
-        p->val = q->val;
-        q->val = temp;
-        dummy = head;
-        if(!q->next){
-            break;
-        }
-        p = q->next;
-        q = p->next;  
+
+    // Below is the solution for swapping values. Look further below for swapping nodes.
+    // struct ListNode *p, *q, *dummy;
+    // int temp;
+    // p = head;
+    // q = head->next;
+    // while(q){
+    //     temp = p->val;
+    //     p->val = q->val;
+    //     q->val = temp;
+    //     dummy = head;
+    //     if(!q->next){
+    //         break;
+    //     }
+    //     p = q->next;
+    //     q = p->next;  
+    // }
+    // return head;
+
+
+    // Below code is solely for swapping nodes
+    struct ListNode *p, *temp;
+    struct ListNode n1;
+    n1.next = head;
+    p=&n1;
+    while(p->next && p->next->next){
+        temp = p->next;
+        p->next = temp->next;
+        temp->next = temp->next->next; 
+        p->next->next = temp; 
+        p = temp;
     }
-    return head;
+    return n1.next;
 }
