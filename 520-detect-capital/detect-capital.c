f62566dd53da22da7c33f4e620da47dcde0f6b7e
Dinesh Bobburu <54496857+bobburudinesh@users.noreply.github.com>
2025-06-17 23:06:04 -0400

    Time: 0 ms (100.00%) | Memory: 7.8 MB (68.73%) - LeetSync

--- 520-detect-capital/detect-capital.c
+++ 520-detect-capital/detect-capital.c
@@ -1,46 +1,16 @@
-typedef enum {
-    case1,
-    case2,
-    case3
-} capitalCases;
+
 
 bool detectCapitalUse(char* word) {
     int length = strlen(word);
-    if(length < 2) {
-        return true;
-    }
-    int i = 0;
-    bool capital = true;
-    capitalCases capitalcases = case1;
-    if(word[0]>= 'a' && word[0]<='z') {
-        capitalcases = case2;
-    } else if (word[0]>= 'A' && word[0]<='Z') {
-        if(word[1]>= 'A' && word[1]<='Z') {
-            capitalcases = case1;
-        } else {
-            capitalcases = case3;
-        }
-    }
-    i = 1;
+    int uppercount = 0, i = 0;
     while(i<length) {
-        switch(capitalcases) {
-            case case1:
-                
-                if(word[i] < 'A' || word[i] > 'Z') {
-                    
-                    capital = false;
-                    break;
-                }
-            break;
-            default:
-                if(word[i] < 'a' || word[i] > 'z') {
-                    printf("%c\n", word[i]);
-                    capital = false;
-                    break;
-                }
-                break;
+        if(isupper(word[i])) {
+            uppercount++;
         }
-       i++;
+        i++;
     }
-    return capital;
+    if(uppercount == length)   return true;
+    if(uppercount == 0) return true;
+    if(uppercount == 1 && isupper(word[0])) return true;
+    return false;
 }
