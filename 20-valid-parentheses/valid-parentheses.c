90c74a7b5d3b28dddf8bd64658a0873ea05927a2
Dinesh Bobburu <54496857+bobburudinesh@users.noreply.github.com>
2025-06-18 23:23:06 -0400

    Time: 0 ms (100.00%) | Memory: 9.1 MB (8.17%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.c
@@ -0,0 +1,42 @@
+#define MAX 10000
+char getClosingBracket(char openingBracket) {
+    if(openingBracket == '(') {
+        return ')';
+    } else if (openingBracket == '[') {
+        return ']';
+    } else {
+        return '}';
+    }
+}
+bool isValid(char* s) {
+    int i = 0, length = strlen(s), j = 0;
+    if(length < 2) {
+        return false;
+    }
+    char arr[MAX] = {};
+    bool result = true;
+    while(i<length) {
+        if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            arr[j] = s[i];
+            j++;
+        } else {
+            if(j>0) {
+                char temp = arr[j-1];
+                j--;
+                if(getClosingBracket(temp) != s[i]) {
+                    result = false;
+                    break;
+                }
+            } else {
+                result = false;
+                break;
+            }
+            
+        }
+        i++;
+    }
+    if(j!=0) {
+        result = false;
+    }
+    return result;
+}
