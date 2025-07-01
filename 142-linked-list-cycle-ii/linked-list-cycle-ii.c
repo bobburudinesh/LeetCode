fe36912305d72f6afc286943ba2b79ca0f720a09
Dinesh Bobburu <54496857+bobburudinesh@users.noreply.github.com>
2025-07-01 17:38:23 -0400

    Time: 8 ms (42.61%) | Memory: 10.3 MB (64.78%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.c
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+void detectCycle_1(struct ListNode *head){
+    
+    struct ListNode *p1 = head, *p2 = head;
+    p2 = p2->next->next;
+    while(p1 && p2) {
+        if(p1 == p2) {
+            printf("Loop Found\n");
+            return;
+        }
+        p1 = p1->next;
+        p2 = (p2->next) ? p2->next->next : NULL;
+    }
+    printf("Loop Not Found\n");
+
+}
+struct ListNode *detectCycle(struct ListNode *head) {
+    if(!head || head->next == NULL) {
+        return NULL;
+    }
+    struct ListNode *p1 = head, *p2 = head;
+    
+    while(p2 && p2->next) {
+        p1 = p1->next;
+        p2 =  p2->next->next;
+        if(p1 == p2) {
+            while(head != p1) {
+                head = head->next;
+                p1 = p1->next;
+            }
+            return head;
+        }
+
+    }
+
+    return NULL;
+}
