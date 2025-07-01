d5520a4ba5f4dd9ebc212fc5e31839f534911e01
Dinesh Bobburu <54496857+bobburudinesh@users.noreply.github.com>
2025-07-01 10:45:20 -0400

    Time: 0 ms (100.00%) | Memory: 7.6 MB (83.39%) - LeetSync

--- 190-reverse-bits/reverse-bits.c
+++ 190-reverse-bits/reverse-bits.c
@@ -5,11 +5,10 @@ uint32_t reverseBits(uint32_t n) {
     int i = 0;
     uint32_t q = 0;
     while(n) {
-        q = q<<1;
-        q |= (n & 1);
+        q |= (n & 1) << (31-i);
         n = n>>1;
         i++;
     }
-    q <<= (32-i);
+    //q <<= (32-i);
     return q;
 }
