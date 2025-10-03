90f314494cdb9511b817a8def87046a6e4c01a60
Dinesh Bobburu <54496857+bobburudinesh@users.noreply.github.com>
2025-10-03 14:58:58 -0500

    Time: 0 ms (100.00%) | Memory: 95.3 MB (48.15%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+            if (nums1.size() > nums2.size()) {
+        return findMedianSortedArrays(nums2, nums1);
+    }
+    
+    int x = nums1.size();
+    int y = nums2.size();;
+    int low = 0, high = x;
+    
+    while (low <= high) {
+        int partitionX = (low + high) / 2;
+        int partitionY = (x + y + 1) / 2 - partitionX;
+        
+        int maxX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+        int minX = (partitionX == x) ? INT_MAX : nums1[partitionX];
+        
+        int maxY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+        int minY = (partitionY == y) ? INT_MAX : nums2[partitionY];
+        
+        if (maxX <= minY && maxY <= minX) {
+            if ((x + y) % 2 == 0) {
+                return ((double)(fmax(maxX, maxY) + fmin(minX, minY))) / 2;
+            } else {
+                return (double)fmax(maxX, maxY);
+            }
+        } else if (maxX > minY) {
+            high = partitionX - 1;
+        } else {
+            low = partitionX + 1;
+        }
+    }
+    
+    return -1; // If input arrays are not sorted, this is an error case.
+    }
+};
