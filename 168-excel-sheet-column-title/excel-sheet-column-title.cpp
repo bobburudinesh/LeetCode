071a6f86c83935dc5135e985a579d0d4594847bf
Dinesh Bobburu <54496857+bobburudinesh@users.noreply.github.com>
2026-07-19 18:48:39 -0400

    Time: 0 ms (100.00%) | Memory: 7.8 MB (58.26%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -4,15 +4,15 @@ public:
         std::string res = "";
         // 65 -> A ---> 1
         int ascii_base = static_cast<int>('A') - 1;  //handing 1 based indexing based on Ascii value
-        int column_num = columnNumber;
-        while(column_num) {
-            int rem = column_num % 26;
+        
+        while(columnNumber) {
+            int rem = columnNumber % 26;
             if(rem == 0) {
                 res.append(1, 'Z');
             } else {
                 res.append(1, static_cast<char>(ascii_base + rem));
             }
-            column_num = (column_num - ((rem == 0) ? 26 : rem))/26;
+            columnNumber = (columnNumber - ((rem == 0) ? 26 : rem))/26;
         }
         std::reverse(res.begin(), res.end());
         return res;
