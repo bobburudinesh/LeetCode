7d20439042f39f29330f2af97fff0a45936d1051
Dinesh Bobburu <54496857+bobburudinesh@users.noreply.github.com>
2026-07-19 19:46:43 -0400

    Time: 0 ms (100.00%) | Memory: 9.5 MB (7.59%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+        std::unordered_map<char,char> iso_map1;
+        std::unordered_map<char,char> iso_map2;
+        for(int i = 0; i < s.length(); i++) {
+            if(iso_map1.contains(s[i])) {
+                if(iso_map1[s[i]] != t[i]) {
+                    return false;
+                }
+            } else if (iso_map2.contains(t[i])) {
+                if(iso_map2[t[i]] != s[i]) {
+                    return false;
+                }
+            } else {
+                    iso_map1.insert({s[i], t[i]});
+                    iso_map2.insert({t[i], s[i]});
+            }
+        }
+
+        return true;
+
+    }
+};
