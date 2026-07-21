609acd79cad85d5c9de78c8273efb704c790142d
Dinesh Bobburu <54496857+bobburudinesh@users.noreply.github.com>
2026-07-21 09:29:19 -0400

    Time: 0 ms (100.00%) | Memory: 12 MB (29.93%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        if((ransomNote.length() > magazine.length())) {
-            return false;
-        }
         std::unordered_map<char, int> map1;
         for (char c : magazine) {
-            //if(map1.contains(c)) {
-                map1[c]++;
-            //}
+            map1[c]++;
+            
         }
         for(char c : ransomNote) {
-
             if(map1[c] >= 1) {
                 map1[c]--;
             } else {
