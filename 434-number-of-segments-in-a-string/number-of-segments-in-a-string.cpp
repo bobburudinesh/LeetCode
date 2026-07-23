9f5c1ff7ed6e59fd7da0a42eaf92c7d55e882d11
Dinesh Bobburu <54496857+bobburudinesh@users.noreply.github.com>
2026-07-22 21:17:24 -0400

    Time: 0 ms (100.00%) | Memory: 8.2 MB (56.10%) - LeetSync

--- 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
+++ 434-number-of-segments-in-a-string/number-of-segments-in-a-string.cpp
@@ -3,21 +3,34 @@ public:
     int countSegments(string s) {
         int segs = 0;
         int seg_len = 0;
-        auto it = s.begin();
-        while(it < s.end()) {
-            if(*it != ' ') {
-                seg_len++;
+        //auto it = s.begin();
+        // while(it < s.end()) {
+        //     if(*it != ' ') {
+        //         seg_len++;
                 
+        //     } else {
+        //         if(seg_len > 0) {
+        //             segs++;
+        //             seg_len = 0;
+        //         }
+        //     }
+        //     ++it;
+
+        // }
+        // if((it == s.end()) && seg_len > 0) {
+        //     segs++;
+        // }
+        for (char c : s) {
+            if(c != ' ') {
+                seg_len++;
             } else {
                 if(seg_len > 0) {
                     segs++;
                     seg_len = 0;
                 }
             }
-            ++it;
-
         }
-        if((it == s.end()) && seg_len > 0) {
+        if(seg_len > 0) {
             segs++;
         }
         return segs;
