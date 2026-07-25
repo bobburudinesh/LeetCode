19c852e8a9a0e2ef709c05ac43a5a6e9c9b88ecc
Dinesh Bobburu <54496857+bobburudinesh@users.noreply.github.com>
2026-07-24 22:05:55 -0400

    Time: 3 ms (16.11%) | Memory: 9.4 MB (18.75%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        std::unordered_map <char, int> map;
+        for(char c : s) {
+            map[c]++;
+        }
+        int len = 0;
+        bool odd_present = false;
+        for(auto m : map) {
+            len += (m.second/2) * 2;
+            if(m.second & 0x1) {
+                odd_present = true;
+            }
+        }
+        
+        len = len + (odd_present ? 1 : 0);
+
+        return len;
+    }
+};
